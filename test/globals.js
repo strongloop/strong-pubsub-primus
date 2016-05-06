7914d00c75274b6dfaa8785e7679161bd1ecd7b2
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 15:40:36 -0700

    update copyright notices and license

--- test/globals.js
+++ test/globals.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: strong-pubsub-primus
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var path = require('path');
 
 global.chai = require('chai');
